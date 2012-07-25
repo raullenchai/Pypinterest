684c36f47defab8e92f0d11ae98dc80f9b42192c
Raullen <raullenchai@gmail.com>
2012-07-25 15:10:47 -0700

    tidy the code, add new functions

--- __init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = '0.1'
-__author__ = "Raullen Chai <raullen@live.ca>"
-__doc__ = """
-Pypinpinpin: An Unofficial (and Somewhat Dirty) Python Wrapper for Pinterest. See README.md for usage instructions.
-"""
